0ee719198272e87d5dce1cc3357e51aa700c5c10
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2023-04-19 21:27:13 +1200

    Cleaned code and shit

--- src/main/java/apollointhehouse/SpeedyPaths/mixin/SpeedyPathsMixin.java
+++ src/main/java/apollointhehouse/SpeedyPaths/mixin/SpeedyPathsMixin.java
@@ -8,25 +8,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Block.class)
 public class SpeedyPathsMixin {
+
     @Inject(method = "onEntityWalking", at = @At("HEAD"), remap = false)
     public void onEntityWalking(World world, int x, int y, int z, Entity entity, CallbackInfo ci) {
 
+        // Check if the block the entity is walking on is cobblestone
         int blockX = MathHelper.floor_double(entity.posX);
         int blockY = MathHelper.floor_double(entity.posY - 0.20000000298023224D - (double)entity.yOffset);
         int blockZ = MathHelper.floor_double(entity.posZ);
+        boolean isCobblestone = world.getBlockId(blockX, blockY, blockZ) == Block.cobbleStone.blockID;
 
-        if (world.getBlockId(blockX, blockY, blockZ) == Block.cobbleStone.blockID && entity.motionX != 0 && entity.motionZ != 0) {
-            double speedLimit = 1; // Set the speed limit to 1 blocks per tick
+        // Check if the entity is moving
+        boolean isMoving = entity.motionX != 0 && entity.motionZ != 0;
+
+        // Apply speed boost if the entity is on cobblestone and moving
+        if (isCobblestone && isMoving) {
+
+            // Calculate the new speed and limit it to 1 block per tick
             double newSpeedX = entity.motionX * 3.0; // Triple the entity's speed
             double newSpeedZ = entity.motionZ * 3.0;
             double newSpeed = Math.sqrt(newSpeedX * newSpeedX + newSpeedZ * newSpeedZ); // Calculate the new speed
+            double speedLimit = 1.0; // Set the speed limit to 1 block per tick
+
+            // If the new speed exceeds the limit, normalize the speed vector and multiply it by the limit
             if (newSpeed > speedLimit) {
-                // If the new speed exceeds the limit, normalize the speed vector and multiply it by the limit
                 double normalizationFactor = speedLimit / newSpeed;
                 entity.motionX *= normalizationFactor;
                 entity.motionZ *= normalizationFactor;
-            } else {
-                // Otherwise, set the entity's new speed
+            }
+            // Otherwise, set the entity's new speed
+            else {
                 entity.motionX = newSpeedX;
                 entity.motionZ = newSpeedZ;
             }
